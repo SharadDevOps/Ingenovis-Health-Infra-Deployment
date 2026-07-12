0e773b2db3f95b7282b22cbf97dc8b0cdf8201a9
DevOps-Sharad <150069621+SharadDevOps@users.noreply.github.com>
2026-07-12 13:28:59 +0530

    feat: add recursive Terraform formatting hook and update output descriptions

--- deployment/projects/platform/hub/outputs.tf
+++ deployment/projects/platform/hub/outputs.tf
@@ -1,11 +1,14 @@
 output "vnet_id" {
-  value = module.hub_networking.vnet_id
+  description = "Id of Virtual Network"
+  value       = module.hub_networking.vnet_id
 }
 
 output "vnet_name" {
-  value = module.hub_networking.vnet_name
+  description = "Name of Virtual Network"
+  value       = module.hub_networking.vnet_name
 }
 
 output "resource_group_name" {
-  value = module.hub_networking.resource_group_name
+  description = "Name of the resource group"
+  value       = module.hub_networking.resource_group_name
 }
